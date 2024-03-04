e80183d165b25b1ad22dcc5135b52b427b9a7dd1
Juan Brizuela <juanbrizuelago@gmail.com>
2024-03-04 15:58:34 -0600

    Refactor code to improve performance and readability

---
+++ app/about/page.tsx
@@ -0,0 +1,31 @@
+async function getHello() {
+  const res = await fetch(
+    `https://tutorialsencillonext-js.vercel.app/api/hello`,
+    {
+      cache: "no-cache",
+    }
+  );
+  return res.json();
+}
+
+type Hello = {
+  message: string;
+  bruh: string;
+  lmao: string;
+};
+
+export default async function About() {
+  const hello: Hello = await getHello();
+
+  const { message, bruh, lmao } = hello;
+
+  console.log(message);
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-tr from-indigo-300 to-indigo-700">
+      <h1 className="text-4xl tracking-tight font-bold">About {message}</h1>
+      <p>{bruh}</p>
+      <p>{lmao}</p>
+    </div>
+  );
+}
---
+++ app/food/[id]/page.tsx
@@ -0,0 +1,38 @@
+async function getFood(id: string) {
+  const res = await fetch(
+    `https://tutorialsencillonext-js.vercel.app/api/food/getFood/${id}`,
+    {
+      cache: "no-cache",
+    }
+  );
+  const food = await res.json();
+  return food;
+}
+
+type Food = {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export default async function Food({ params }: { params: { id: string } }) {
+  const food: Food = await getFood(params.id);
+
+  console.log(food);
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col items-center p-10 bg-white shadow-2xl my-10 text-black gap-8 rounded-lg">
+        <h1 className="text-3xl font-semibold">{food.name}</h1>
+
+        <img src={food.image} alt={food.name} width={100} height={100} />
+        <p>{food.description}</p>
+        <p>${food.price}</p>
+      </div>
+    </div>
+  );
+}
